d447b67d955f59454f0e85f08606ead3594acca4
yannelly25 <yannellym@gmail.com>
2022-10-27 16:24:19 -0400

    methods example

---
+++ practice/src/Greetings.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Greetings {
+    public static void main(String[] args) {
+        System.out.println("Enter your name: ");
+        Scanner scanner = new Scanner(System.in);
+        String name = scanner.next();
+        greetUser(name);
+        scanner.close();
+    }
+    public static void greetUser(String name){
+        System.out.println("Hi there, " + name);
+    }
+}
---
+++ practice/src/InstantCreditCheck.java
@@ -0,0 +1,50 @@
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
+public class InstantCreditCheck {
+
+    static int requiredSalary = 25000;
+    static int requiredCreditScore = 700;
+    static Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        double salary = getSalary();
+        int creditScore = getCreditScore();
+        scanner.close();
+        boolean qualified = isUserQualified(creditScore, salary);
+        notifyUser(qualified);
+    }
+    public static void getFormData(){
+        System.out.println("Enter your salary: ");
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter your credit score: ");
+        int creditScore = scanner.nextInt();
+        scanner.close();
+    }
+    public static double getSalary(){
+        System.out.println("Enter your salary: ");
+        double salary = scanner.nextDouble();
+        return salary;
+    }
+    public static int getCreditScore(){
+        System.out.println("Enter your credit score: ");
+        int creditScore = scanner.nextInt();
+        return creditScore;
+    }
+    public static boolean isUserQualified(int creditScore, double salary){
+        if(creditScore >= requiredCreditScore && salary >= requiredSalary){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+    public static void notifyUser( boolean isQualified){
+        if(isQualified){
+            System.out.println("Congrats! you have been approved.");
+        } else{
+            System.out.println("Sorry, you have been declined");
+        }
+
+    }
+}
